71c62f5fb93afdfb9aa2af1a043e5aecf836e11d
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2014-05-04 12:03:17 +0900

    Scala版新規作成

---
+++ ProgramInScala.scala
@@ -0,0 +1,38 @@
+/*
+ * Scalaによるプログラム
+ * author: OHASHI Kent
+ */
+
+import scala.math.sqrt
+
+object ProgramInScala {
+  private val FirstPrime: Int = 2
+
+  /*
+   * エラトステネスの篩(ふるい)により最大値maxまでの素数のリストを取得する。
+   * maxが整数でない場合、コンパイルエラーとなる。
+   */
+  def primeNumbers(max: Int): List[Int] = {
+    val numbers = List.range(FirstPrime, max+1)
+    val primes = List()
+    val stopPoint = sqrt(max).toInt
+    primeFilter(numbers, primes, stopPoint)
+  }
+
+  private def primeFilter(numbers: List[Int], primes: List[Int], stopPoint: Int): List[Int] = {
+    numbers match {
+      case Nil     => List()
+      case n :: ns =>
+        if (n > stopPoint) {
+          primes.reverse ::: numbers
+        } else {
+          primeFilter(ns.filter(_ % n != 0), n :: primes, stopPoint)
+        }
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    // 実行例
+    println(primeNumbers(100))
+  }
+}
