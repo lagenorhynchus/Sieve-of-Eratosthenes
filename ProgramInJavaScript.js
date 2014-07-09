6b8e39f994598a6ccbcfcf2ead0f7b34e7295619
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2014-07-09 23:16:52 +0900

    モジュールパターンを適用

--- ProgramInJavaScript.js
+++ ProgramInJavaScript.js
@@ -1,49 +1,53 @@
 // JavaScriptによるプログラム
 // author: OHASHI Kent
 
-var FIRST_PRIME = 2;
+var ProgramInJavaScript = (function () {
+    var FIRST_PRIME = 2;
 
-// エラトステネスの篩(ふるい)により最大値maxまでの素数のリストを取得する。
-// maxが整数でない場合、TypeErrorを発生させる。
-var primeNumbers = function (max) {
-    var numbers = [];
-    var stopPoint;
-    var i;
-
-    if (typeof max !== "number" || !isFinite(max) || !max.toString().match(/^[+-]?\d+$/)) {
-        throw {
-            name: "TypeError",
-            message: "max must be an integer"
+    var primeFilter = function (numbers, stopPoint) {
+        var primes = [];
+        var n;
+        var isCandidate = function (x) {
+            return x % n !== 0;
         };
-    }
 
-    if (max < FIRST_PRIME) {
-        return [];
-    }
-
-    for (i = FIRST_PRIME; i <= max; i += 1) {
-        numbers.push(i);
-    }
-    stopPoint = Math.floor(Math.sqrt(max));
-    return primeFilter(numbers, stopPoint);
-};
-
-var primeFilter = function (numbers, stopPoint) {
-    var primes = [];
-    var n;
-    var isCandidate = function (x) {
-        return x % n !== 0;
+        for (n = FIRST_PRIME; n <= stopPoint; n += 1) {
+            if (numbers[0] === n) {
+                primes.push(n);
+                numbers = numbers.slice(1).filter(isCandidate);
+            }
+        }
+        primes = primes.concat(numbers);
+        return primes;
     };
 
-    for (n = FIRST_PRIME; n <= stopPoint; n += 1) {
-        if (numbers[0] === n) {
-            primes.push(n);
-            numbers = numbers.slice(1).filter(isCandidate);
+    return {
+        // エラトステネスの篩(ふるい)により最大値maxまでの素数のリストを取得する。
+        // maxが整数でない場合、TypeErrorを発生させる。
+        primeNumbers: function (max) {
+            var numbers = [];
+            var stopPoint;
+            var i;
+
+            if (typeof max !== "number" || !isFinite(max) || !max.toString().match(/^[+-]?\d+$/)) {
+                throw {
+                    name: "TypeError",
+                    message: "max must be an integer"
+                };
+            }
+
+            if (max < FIRST_PRIME) {
+                return [];
+            }
+
+            for (i = FIRST_PRIME; i <= max; i += 1) {
+                numbers.push(i);
+            }
+            stopPoint = Math.floor(Math.sqrt(max));
+            return primeFilter(numbers, stopPoint);
         }
-    }
-    primes = primes.concat(numbers);
-    return primes;
-};
+    };
+}());
 
 // 実行例
-console.log(primeNumbers(100));
+console.log(ProgramInJavaScript.primeNumbers(100));
--- ProgramInJavaScriptUnderscore.js
+++ ProgramInJavaScriptUnderscore.js
@@ -1,44 +1,48 @@
 // JavaScript (Underscore.js)によるプログラム
 // author: OHASHI Kent
 
-var FIRST_PRIME = 2;
+var ProgramInJavaScriptUnderscore = (function () {
+    var FIRST_PRIME = 2;
 
-// エラトステネスの篩(ふるい)により最大値maxまでの素数のリストを取得する。
-// maxが整数でない場合、TypeErrorを発生させる。
-var primeNumbers = function (max) {
-    var numbers;
-    var primes = [];
-    var stopPoint;
+    var primeFilter = function (numbers, primes, stopPoint) {
+        var n;
 
-    if (typeof max !== "number" || !isFinite(max) || !max.toString().match(/^[+-]?\d+$/)) {
-        throw {
-            name: "TypeError",
-            message: "max must be an integer"
-        };
-    }
+        if (_.isEmpty(numbers)) {
+            return [];
+        }
 
-    numbers = _.range(FIRST_PRIME, max + 1);
-    stopPoint = Math.floor(Math.sqrt(max));
-    return primeFilter(numbers, primes, stopPoint);
-};
+        n = _.first(numbers);
+        if (n > stopPoint) {
+            return primes.concat(numbers);
+        } else {
+            primes.push(n);
+            return primeFilter(_.filter(_.rest(numbers), function (x) {
+                return x % n !== 0;
+            }), primes, stopPoint);
+        }
+    };
 
-var primeFilter = function (numbers, primes, stopPoint) {
-    var n;
+    return {
+        // エラトステネスの篩(ふるい)により最大値maxまでの素数のリストを取得する。
+        // maxが整数でない場合、TypeErrorを発生させる。
+        primeNumbers: function (max) {
+            var numbers;
+            var primes = [];
+            var stopPoint;
 
-    if (_.isEmpty(numbers)) {
-        return [];
-    }
+            if (typeof max !== "number" || !isFinite(max) || !max.toString().match(/^[+-]?\d+$/)) {
+                throw {
+                    name: "TypeError",
+                    message: "max must be an integer"
+                };
+            }
 
-    n = _.first(numbers);
-    if (n > stopPoint) {
-        return primes.concat(numbers);
-    } else {
-        primes.push(n);
-        return primeFilter(_.filter(_.rest(numbers), function (x) {
-            return x % n !== 0;
-        }), primes, stopPoint);
-    }
-};
+            numbers = _.range(FIRST_PRIME, max + 1);
+            stopPoint = Math.floor(Math.sqrt(max));
+            return primeFilter(numbers, primes, stopPoint);
+        }
+    };
+}());
 
 // 実行例
-console.log(primeNumbers(100));
+console.log(ProgramInJavaScriptUnderscore.primeNumbers(100));
