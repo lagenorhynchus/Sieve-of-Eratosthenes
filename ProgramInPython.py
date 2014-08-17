01f56911ac0ffefb7b9641417c587ef75b8ed6e1
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2014-08-17 16:01:01 +0900

    変数・関数をprivate化

--- ProgramInPython.py
+++ ProgramInPython.py
@@ -5,7 +5,7 @@
 
 from math import sqrt
 
-FIRST_PRIME = 2
+_FIRST_PRIME = 2
 
 
 # エラトステネスの篩(ふるい)により最大値mまでの素数のリストを取得する。
@@ -14,17 +14,17 @@ def prime_numbers(m):
     if not isinstance(m, int):
         raise TypeError("m must be an integer")
 
-    if m < FIRST_PRIME:
+    if m < _FIRST_PRIME:
         return []
 
-    numbers = list(range(FIRST_PRIME, m + 1))
+    numbers = list(range(_FIRST_PRIME, m + 1))
     stop_point = int(sqrt(m))
-    return prime_filter(numbers, stop_point)
+    return _prime_filter(numbers, stop_point)
 
 
-def prime_filter(numbers, stop_point):
+def _prime_filter(numbers, stop_point):
     primes = []
-    for n in range(FIRST_PRIME, stop_point + 1):
+    for n in range(_FIRST_PRIME, stop_point + 1):
         if numbers[0] == n:
             primes.append(n)
             numbers = [x for x in numbers[1:] if x % n != 0]
