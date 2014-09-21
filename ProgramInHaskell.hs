26e3a3351de8b57389bf9f1f06b4e303188dd940
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2014-09-21 17:10:02 +0900

    インデントスタイルを変更

--- ProgramInHaskell.hs
+++ ProgramInHaskell.hs
@@ -4,8 +4,8 @@ author: OHASHI Kent
 -}
 
 module ProgramInHaskell (
-    primeNumbers,
-    main
+  primeNumbers,
+  main
 ) where
 
 firstPrime :: Int
@@ -15,15 +15,16 @@ firstPrime = 2
 -- mが整数でない場合、コンパイルエラーとなる。
 primeNumbers :: Int -> [Int]
 primeNumbers m = primeFilter numbers primes stopPoint
-    where numbers   = [firstPrime..m]
-          primes    = []
-          stopPoint = floor . sqrt $ (fromIntegral m :: Double)
+  where
+    numbers   = [firstPrime..m]
+    primes    = []
+    stopPoint = floor . sqrt $ (fromIntegral m :: Double)
 
 primeFilter :: [Int] -> [Int] -> Int -> [Int]
 primeFilter [] _ _ = []
 primeFilter numbers@(n:ns) primes stopPoint
-    | n > stopPoint = reverse primes ++ numbers
-    | otherwise     = primeFilter [x | x <- ns, x `mod` n /= 0] (n : primes) stopPoint
+  | n > stopPoint = reverse primes ++ numbers
+  | otherwise     = primeFilter [x | x <- ns, x `mod` n /= 0] (n : primes) stopPoint
 
 -- 実行例
 main :: IO ()
